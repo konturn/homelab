b7c0742a1763ba57257656f760bf58b7404643cb
Administrator <admin@example.com>
2022-07-03 21:51:04 -0400

    refactor to use ansible variables rather than env var lookups

--- networking/iptables/ip_update.sh
+++ networking/iptables/ip_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ip=$(ip -f inet addr show {{ lookup('env', 'INET_IF') }}|sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+ip=$(ip -f inet addr show {{ inet_interface_name }}|sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 ipset flush wan_ip
 ipset add wan_ip $ip
