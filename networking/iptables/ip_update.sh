8a0f547e9772636be84d74278478d7d3448f6918
Administrator <admin@example.com>
2022-07-03 22:26:49 -0400

    fix: various misconfigs in cron scripts

--- networking/iptables/ip_update.sh
+++ networking/iptables/ip_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ip=$(ip -f inet addr show {{ inet_interface_name }}|sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
-ipset flush wan_ip
-ipset add wan_ip $ip
+ip=$(/sbin/ip -f inet addr show enp2s0f0|sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+/sbin/ipset flush wan_ip
+/sbin/ipset add wan_ip $ip
