a02721346753744a3833affcdf43feca1a410249
Administrator <admin@example.com>
2022-07-06 15:20:52 -0400

    initial check-in for http automation

--- docker/nginx/generate-configs.py
+++ docker/nginx/generate-configs.py
@@ -10,10 +10,8 @@ def generate_stream_config(port_mappings, path):
         result = json.load(stream)
     with open(path + '/stream-entry-template.conf', 'r') as stream:
         template = json.load(stream)
-    line_counter = 2
     for service_name, port in port_mappings.items():
         template_instance = copy.deepcopy(template)
-        template_instance['line'] = line_counter
         host_port = port
         container_port = port
         port_split = port.split(":")
@@ -22,13 +20,32 @@ def generate_stream_config(port_mappings, path):
             container_port = port_split[1]
         template_instance['block'][0]['args'].append(host_port)
         template_instance['block'][1]['args'].append(service_name + ":" + container_port)
-        template_instance['block'][0]['line'] = line_counter + 1
-        template_instance['block'][1]['line'] = line_counter + 2
-        line_counter += 4
         result['config'][0]['parsed'][0]['block'].append(template_instance)
     with open(path + "/stream-output.conf", "w") as output:
         output.writelines(json.dumps(result))
     cmd = "crossplane build -f -d " + path + " " + path + "/stream-output.conf"
+    os.system(cmd)
+
+def generate_http_config(port_mappings, path, domain_name):
+    with open(path + '/http-master-template.conf', 'r') as http:
+        result = json.load(http)
+    with open(path + '/http-entry-template.conf', 'r') as http:
+        template = json.load(http)
+    for service_name, port in port_mappings.items():
+        template_instance = copy.deepcopy(template)
+        host_port = port
+        container_port = port
+        port_split = port.split(":")
+        if len(port_split) == 2:
+            host_port = port_split[0]
+            container_port = port_split[1]
+        template_instance['block'][0]['args'].append(host_port)
+        template_instance['block'][1]['args'].append(service_name + "." + domain_name)
+        template_instance['block'][3]['block'][0]['args'].append("http://" + service_name + ":" + container_port)
+        result['config'][0]['parsed'][0]['block'].append(template_instance)
+    with open(path + "/http-output.conf", "w") as output:
+        output.writelines(json.dumps(result))
+    cmd = "crossplane build -f -d " + path + " " + path + "/http-output.conf"
     os.system(cmd)
 
 def generate_port_mappings(services, network):
@@ -50,9 +67,6 @@ def main():
     parser.add_argument('--workspace-path', type=str,
                         help='The path which the script should use as a workspace',
                         required=True)
-#    parser.add_argument('--http-config-path', type=str,
-#                        help='The path of the directory where the http config file resides',
-#                        required=True)
     parser.add_argument('--network', type=str,
                         help='The name of the docker network which the nginx instance serves for',
                         required=True)
@@ -61,7 +75,13 @@ def main():
     with open(args.workspace_path + "/docker-compose.yml", "r") as stream:
         services = yaml.safe_load(stream)['services']
 
+    if args.network == "internal":
+        domain_name = "lab.nkontur.com"
+    else:
+        domain_name = "nkontur.com"
+
     stream_port_mappings, http_port_mappings = generate_port_mappings(services, args.network)
+    generate_http_config(http_port_mappings, args.workspace_path, domain_name)
     generate_stream_config(stream_port_mappings, args.workspace_path)
 
 
