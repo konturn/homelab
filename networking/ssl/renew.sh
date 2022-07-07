fc91b96e0f5c893500b0db4596c8670ba717ace2
Administrator <admin@example.com>
2022-07-07 17:14:53 -0400

    fix: set docker network in renew script

--- networking/ssl/renew.sh
+++ networking/ssl/renew.sh
@@ -1,4 +1,4 @@
-docker run --rm -e "NAMESILO_API_KEY=$1" -e "NAMESILO_PROPAGATION_TIMEOUT=100000" -v {{ docker_persistent_data_path }}/certs/nkontur.com:/ssl goacme/lego --dns namesilo --domains *.nkontur.com --domains nkontur.com --domains *.lab.nkontur.com --email konoahko@gmail.com --dns.resolvers 1.1.1.1 --path /ssl -a run
+docker run --rm -e "NAMESILO_API_KEY=$1" -e "NAMESILO_PROPAGATION_TIMEOUT=100000" --network docker_internal -v {{ docker_persistent_data_path }}/certs/nkontur.com:/ssl goacme/lego --dns namesilo --domains *.nkontur.com --domains nkontur.com --domains *.lab.nkontur.com --email konoahko@gmail.com --dns.resolvers 1.1.1.1 --path /ssl -a run
 scp -i /root/.ssh/id_rsa {{ docker_persistent_data_path }}/certs/nkontur.com/certificates/_.nkontur.com.crt root@vps.nkontur.com:/etc/ssl/certs/nkontur.com/_.nkontur.com.crt
 scp -i /root/.ssh/id_rsa {{ docker_persistent_data_path }}/certs/nkontur.com/certificates/_.nkontur.com.key root@vps.nkontur.com:/etc/ssl/private/_.nkontur.com.key
 ssh -i /root/.ssh/id_rsa vps.nkontur.com "chown konoahko:konoahko /etc/ssl/certs/nkontur.com/_.nkontur.com.crt && chown root:ssl-cert /etc/ssl/private/_.nkontur.com.key"
