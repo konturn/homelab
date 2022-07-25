aa8859476f758a2e125825f757671ca7c0ee848f
Administrator <admin@example.com>
2022-07-24 22:25:01 -0400

    feat: refactor ddns script to use argparse and support multiple domains; update wildcard record

--- networking/dns/ddns.py
+++ networking/dns/ddns.py
@@ -1,51 +1,41 @@
 import requests
 import xml.etree.ElementTree as ET
 import sys
-import getopt
-def usage():
-    print('python3 ddns.py -d DOMAIN -k KEY -r RRHOST [-h]')
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'd:k:rh', ['domain=', 'key=', 'rrhost=', 'help'])
-except getopt.GetoptError:
-    usage()
-    sys.exit(2)
+import argparse
 
-domain = ''
-key = ''
-rrhost = ''
-if not opts:
-    usage()
-    sys.exit(2)
-for opt, arg in opts:
-    if opt in ('-h', '--help'):
-        usage()
-        sys.exit(2)
-    elif opt in ('-d', '--domain'):
-        domain = arg
-    elif opt in ('-k', '--key'):
-        key = arg
-    elif opt in ('-r', '--rrhost'):
-        rrhost = arg
-    else:
-        usage()
-        sys.exit(2)
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--domain', type=str,
+        help='The domain to update DNS records for',
+        required=True)
+parser.add_argument('-k', '--key', type=str,
+        help='The value of the Namesilo API key',
+        )
+parser.add_argument('-r', '--rrhost', type=str,
+                    help='The name of the rrhost',
+                    nargs='+',
+                    required=False)
+args = parser.parse_args()
+domain=args.domain
+key = args.key
+rrhost = args.rrhost
 
 my_ip = requests.get('https://ifconfig.me').text
-list_request = 'https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=' + key + '&domain=' + domain
+list_request = 'https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=' + key + '&domain=' + "nkontur.com"
 response = requests.get(list_request);
 xml = response.text
 root = ET.fromstring(xml)
-for record in root.iter('resource_record'):
-   record_type = record.find('type').text
-   if record_type == 'A':
-       record_host = record.find('host').text
-       if record_host == rrhost + domain:
-           rrid = record.find('record_id').text
-           update_request = 'https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=' + key + '&domain=' + domain + '&rrid=' + rrid + '&rrhost=' + rrhost + '&rrvalue=' + my_ip + '&rrttl=7207'
-           response = requests.get(update_request)
-           code = response.status_code
-           if code!=200:
-               print('Update failed--Namesilo returned status code ' + str(code))
-           else:
-               print(response.content)
-
+for host in rrhost:
+    for record in root.iter('resource_record'):
+       record_type = record.find('type').text
+       if record_type == 'A':
+           record_host = record.find('host').text
+           if record_host == host + "." + domain or record_host == host + domain:
+               rrid = record.find('record_id').text
+               update_request = 'https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=' + key + '&domain=' + domain + '&rrid=' + rrid + '&rrhost=' + host + '&rrvalue=' + my_ip + '&rrttl=7207'
+               response = requests.get(update_request)
+               code = response.status_code
+               if code!=200:
+                   print('Update failed--Namesilo returned status code ' + str(code))
+               else:
+                   print(response.content)
+    
