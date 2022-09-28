94b04c6253099140935dd886a847ce6fd40bce10
Administrator <admin@example.com>
2022-09-28 16:14:41 -0400

    play empty sound as part of rhasspy entry so that audio doesn't lag

---
+++ docker/rhasspy/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nohup play -n &
+mkdir -p /var/cache/apt/arm64/archives/partial /var/cache/apt/amd64/archives/partial
+apt-get autoclean
+apt install -y netcat
+/usr/lib/rhasspy/bin/rhasspy-voltron --user-profiles /profiles --profile en
