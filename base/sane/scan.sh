81682b7cccb1d93820a0def55d21014494b6fe77
Administrator <admin@example.com>
2022-10-26 19:17:41 -0400

    remove scanbd and update scan script injection

--- base/sane/scan.sh
+++ base/sane/scan.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-now=`date +"%Y-%m-%d-%H%M"`
-/root/sane-scan-pdf/scan --device 'fujitsu:ScanSnap iX1600:144412' -d -r 300 -v -m Lineart --skip-empty-pages -o /root
+now=`date +"%Y-%m-%d-%H%M%S"`
+/root/sane-scan-pdf/scan --device 'fujitsu:ScanSnap iX1600:144412' -r 600 -m Lineart -o /mnt/scan-${now}.pdf
+
