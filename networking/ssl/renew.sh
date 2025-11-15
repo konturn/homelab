3051f8e3a294ce7e97e6027f848727aae0ee22be
Administrator <admin@example.com>
2025-11-15 18:14:05 -0500

    nuke vps

--- networking/ssl/renew.sh
+++ networking/ssl/renew.sh
@@ -11,16 +11,11 @@ docker run --rm \
   -d *.iot.lab.nkontur.com \
   -d *.lab.nkontur.com \
   -d nkontur.com \
-  -d vps.nkontur.com \
   -d homeassistant.nkontur.com \
   -d blog.nkontur.com \
   -d bitwarden.nkontur.com \
   -d booksonic.nkontur.com \
   -v
-scp -i /root/.ssh/id_rsa /persistent_data/application/certs/nkontur.com/live/iot.lab.nkontur.com-0001/cert.pem root@vps.nkontur.com:/etc/ssl/certs/nkontur.com/_.nkontur.com.crt
-scp -i /root/.ssh/id_rsa /persistent_data/application/certs/nkontur.com/live/iot.lab.nkontur.com-0001/privkey.pem root@vps.nkontur.com:/etc/ssl/private/_.nkontur.com.key
 rsync -avz -e 'ssh -i /root/.ssh/id_rsa' /persistent_data/application/certs/nkontur.com/live/iot.lab.nkontur.com-0001 root@zwave.lab.nkontur.com:/certs/
-ssh -i /root/.ssh/id_rsa vps.nkontur.com "chown konoahko:konoahko /etc/ssl/certs/nkontur.com/_.nkontur.com.crt && chown root:ssl-cert /etc/ssl/private/_.nkontur.com.key"
-ssh -i /root/.ssh/id_rsa vps.nkontur.com "systemctl restart postfix dovecot"
 docker restart lab_nginx
 docker restart nginx
