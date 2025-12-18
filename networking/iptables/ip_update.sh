90d949faa28e590fb8024ad063931d1c72c013de
Noah Kontur <konturn@wustl.edu>
2025-12-18 01:23:58 -0500

    fix: ip update script

--- networking/iptables/ip_update.sh
+++ networking/iptables/ip_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ip=$(/sbin/ip -f inet addr show enx6c1ff76b2ec9|sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+ip=$(/sbin/ip -f inet addr show {{ inet_interface_name }}|sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 /sbin/ipset flush wan_ip
 /sbin/ipset add wan_ip $ip
